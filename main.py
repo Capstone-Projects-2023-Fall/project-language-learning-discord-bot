06311ca21dd2b27d3980fc53043452645b87da96
Alan <tuj93905@temple.edu>
2023-09-21 23:52:39 -0400

    Implemented basic functionality of the bot, i.e., if it comes online.

---
+++ main.py
@@ -0,0 +1,22 @@
+import os
+import discord
+from discord.ext import commands
+
+# Intents
+intents = discord.Intents.default()
+intents.message_content = True
+
+# Creating the bot instance
+bot = commands.Bot(command_prefix="!", intents=intents)
+
+
+# Bot events
+@bot.event
+async def on_ready():
+    print(f'{bot.user} has connected to Discord!')
+    print(f"Logged in as {bot.user.name}")
+    print("Bot is now online and ready to use!")
+
+
+# Run the bot
+bot.run(os.environ['BOT_TOKEN'])
