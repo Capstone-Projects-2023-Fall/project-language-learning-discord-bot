18dc2e5a234f751607297bb7789dd8ea44aba85f
Alan <tuj93905@temple.edu>
2023-09-22 00:07:41 -0400

    The bot now sends a message when it comes online to the respected channel.

--- main.py
+++ main.py
@@ -17,6 +17,12 @@ async def on_ready():
     print(f"Logged in as {bot.user.name}")
     print("Bot is now online and ready to use!")
 
+    # Send a message to a specific channel when the bot comes online
+    channel_id = 1154069544629960925  # Replace with specific channel ID
+    channel = bot.get_channel(channel_id)
+
+    if channel:
+        await channel.send("Hello, I'm now online!")
 
 # Run the bot
 bot.run(os.environ['BOT_TOKEN'])
