e8b18fcd0cfc9a78fd8e610f14cefcc11eafdb56
abir itsmeee <akumareign@gmail.com>
2023-09-29 11:40:28 -0400

    patch to fix help command and setup command skeleton added

--- main.py
+++ main.py
@@ -38,26 +38,6 @@ async def on_ready():
     if channel:
         await channel.send("Hello, I'm now online!")
 
-# @bot.event
-# async def on_message(message):
-#     if message.author == bot.user:
-#         return
-#     # type $test to test the db connection
-#     if message.content.startswith('$test'):
-#         await message.channel.send('Hello!')
-#         await message.channel.send(message.author)
-#         # Find user in database
-#         dbuser = database.findUser(str(message.author))
-#         if dbuser is not None:
-#             print(dbuser)
-#         else:
-#             print("Insert new user")
-#             user = {"_id": str(message.author)}
-#             database.insertUser(user)
-#     # other commands go here
-#     if message.content.startswith('$help'):
-#         await message.channel.send('Hello! This is the content of help command')
-
 
 # Loads all the cogs (external commands organized in classes)
 async def setup_hook():
