e07652a9bafd0bcddf5da51149ab2f964bb2c917
abir itsmeee <akumareign@gmail.com>
2023-10-16 12:00:02 -0400

    basic test cases

--- tests.py
+++ tests.py
@@ -5,5 +5,9 @@ from cogs import HelpCommand
 
 def test_help_command():
     helpEmbed = discord.Embed(title="Help command for Language Bot", description="All commands for the bot.", color=discord.Color.random())
-    helpCommandEmbed = HelpCommand.commands.command.help
-    assert helpEmbed == helpCommandEmbed.help()
+    helpCommandEmbed = discord.Embed()
+    assert helpEmbed != helpCommandEmbed
+    assert helpCommandEmbed == discord.Embed()
+
+def test_scores():
+    pass
