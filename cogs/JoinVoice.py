f9f7c65bf97ed190b7d38b7a3dbed6cef65c0891
Kevin <khn@temple.edu>
2023-10-17 21:29:36 -0400

    Added command '!dc' that disconnects the bot, this should be automated in the future

--- cogs/JoinVoice.py
+++ cogs/JoinVoice.py
@@ -11,6 +11,7 @@ class JoinVoice(commands.Cog):
         if ctx.author.voice:
             voice_channel = ctx.author.voice.channel
 
+            # if bot is already connected to a (different) voice channel
             if ctx.voice_client is not None:
                 await ctx.voice_client.move_to(voice_channel)
             else:
@@ -18,6 +19,16 @@ class JoinVoice(commands.Cog):
                 print(f"Bot has joined {voice_channel.name}.")
         else:
             await ctx.send("You need to be in a voice channel to use this command.")
+
+    # command for bot to leave call (this should be automated at some point 
+    # i.e. x seconds when channel is empty)
+    @commands.command()
+    async def dc(self, ctx):
+        if ctx.voice_client is not None:
+            await ctx.voice_client.disconnect()
+            print(f"Bot has left the voice channel.")
+        else:
+            await ctx.send("The bot is not in a voice channel.")
 
 async def setup(bot):
     await bot.add_cog(JoinVoice(bot))
