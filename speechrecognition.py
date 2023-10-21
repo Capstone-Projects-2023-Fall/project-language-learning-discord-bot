364de512e77b762acba204299dc612bee76e8b87
Nhat Doan <74613870+barrydoan@users.noreply.github.com>
2023-10-20 23:32:42 -0400

    add free lib

--- speechrecognition.py
+++ speechrecognition.py
@@ -1,6 +1,7 @@
 import openai
 import os
 from dotenv import load_dotenv
+import speech_recognition as sr
 
 load_dotenv()
 
@@ -13,15 +14,26 @@ class SpeechToText(object):
 
     def initAPI(self) -> None:
         openai.api_key = os.environ['OPENAI_API_KEY']
+        self.r = sr.Recognizer()
 
-    def speech_to_text(self, filename):
+    def speech_to_text(self, filename, language):
         path = "audios/"
-        audio_file= open(path + filename, "rb")
-        transcript = openai.Audio.transcribe("whisper-1", audio_file)
+        #audio_file= open(path + filename, "rb")
+        if language == "English":
+            with sr.AudioFile(path + filename) as source:
+                audio_data = self.r.record(source)
+                text = self.r.recognize_google(audio_data)
+                return text
+        else: 
+            audio_file= open(path + filename, "rb")
+            transcript = openai.Audio.transcribe("whisper-1", audio_file)
         return transcript["text"]
 
 
     
-        
+s2t = SpeechToText()
+text = s2t.speech_to_text(filename="how-are-you.wav", language="English")
+print(text)
+
 
 
