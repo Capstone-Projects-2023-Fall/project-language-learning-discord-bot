f5fa20e075b72b75164b8eb0d257b1b01e9e252c
abir itsmeee <akumareign@gmail.com>
2023-12-04 00:52:27 -0500

    time for kev to try

--- cogs/MyProgress.py
+++ cogs/MyProgress.py
@@ -19,6 +19,7 @@ class MyProgress(commands.Cog):
 
     @commands.command()
     async def myProgress(self, ctx):
+        my_msg = await ctx.send('loading...')
         username = str(ctx.message.author)
         dbuser = database.findUser(username=username)
         if dbuser is not None and constant.USER_LANGUAGE in dbuser:
@@ -28,6 +29,139 @@ class MyProgress(commands.Cog):
             practice_shown = False
             practice_id = ""
             practice_type = ""
+
+            progress_embed = discord.Embed(
+                title=dbunit["name"],
+                description=dbunit["title"],
+                color=discord.Color.random()
+            )
+            backbutton = discord.ui.Button(emoji='◀️', disabled=True)
+            nextbutton = discord.ui.Button(emoji='▶️', disabled=len(dbunit['name']) == 1)
+            endbutton = discord.ui.Button(emoji='❌')
+            idx = 0
+
+            async def backbutton_callback(interaction):
+                nonlocal idx, backbutton, nextbutton, progress_embed
+                idx -= 1
+
+                if idx == 0:
+                    backbutton.disabled = True
+                if idx == len(dbunit['name']) - 2:
+                    nextbutton.disabled = False
+
+                progress_embed.title = dbunit['name'][idx]
+                progress_embed.description = dbunit['title'][idx]
+                dblessons = dbunit["lessons"]
+                for dblesson in dblessons:
+                    if dblesson["isDone"] == True: 
+                        done_count += 1
+                    if practice_id == "" and dblesson["isDone"] == False:
+                        practice_id = dblesson["id"]
+                        practice_type = dblesson["type"]
+
+                    done = "Not completed"
+                    if dblesson["isDone"] == True:
+                        done = "Completed"
+                    embed.add_field(
+                        name=f"Lesson {index + 1}",
+                        value=f"{dblesson['name']}{done}",
+                        inline=False
+                    )
+
+                if done_count < len(dblessons) and practice_shown == False:
+                    async def button_record_callback(interaction):
+                        await interaction.response.send_message("Start practice!")
+                        if practice_type == "quiz":
+                            quiz = database.getRandomQuiz(language)
+                            vocabQuiz = vocabquiz.VocabQuiz(ctx=ctx,user=username, quiz=quiz, progressId=practice_id)
+                            hasQuestion, question, view = vocabQuiz.get_question()
+                            if hasQuestion:
+                                await ctx.send(question, view=view)
+                        elif practice_type == "practice":
+                            practice = database.getRandomPractice(language)
+                            pronunTest = pronuntest.PronunTest(ctx=ctx, user=username, practice=practice, progressId=practice_id)
+                            hasQuestion, sentence, view = pronunTest.get_question()
+                            if hasQuestion:
+                                await ctx.send(f"How do you say: {sentence}", view=view)
+
+                if done_count == len(dblessons):
+                    embed.set_thumbnail(url="attachment://trophy.png")
+                    file = discord.File("images/trophy.png", filename="trophy.png")
+                    await interaction.response.defer()
+                    await my_msg.edit(content='', embed=progress_embed, view=view, file=file)
+                else:
+                    await interaction.response.defer()
+                    await my_msg.edit(content='', embed=progress_embed, view=view)
+
+                button.callback = button_record_callback
+                practice_shown = True
+                print(f"{practice_id}    {practice_type}")
+
+            async def nextbutton_callback(interaction):
+                nonlocal idx, backbutton, nextbutton, progress_embed
+                idx += 1
+
+                if idx == len(dbunit['title']) - 1:
+                    nextbutton.disabled = True
+                if idx == 1:
+                    backbutton.disabled = False
+
+                progress_embed.title = dbunit['name'][idx]
+                progress_embed.description = dbunit['title'][idx]
+                dblessons = dbunit["lessons"]
+                for dblesson in dblessons:
+                    if dblesson["isDone"] == True: 
+                        done_count += 1
+                    if practice_id == "" and dblesson["isDone"] == False:
+                        practice_id = dblesson["id"]
+                        practice_type = dblesson["type"]
+
+                    done = "Not completed"
+                    if dblesson["isDone"] == True:
+                        done = "Completed"
+                    embed.add_field(
+                        name=f"Lesson {index + 1}",
+                        value=f"{dblesson['name']}{done}",
+                        inline=False
+                    )
+
+                if done_count < len(dblessons) and practice_shown == False:
+                    async def button_record_callback(interaction):
+                        await interaction.response.send_message("Start practice!")
+                        if practice_type == "quiz":
+                            quiz = database.getRandomQuiz(language)
+                            vocabQuiz = vocabquiz.VocabQuiz(ctx=ctx,user=username, quiz=quiz, progressId=practice_id)
+                            hasQuestion, question, view = vocabQuiz.get_question()
+                            if hasQuestion:
+                                await ctx.send(question, view=view)
+                        elif practice_type == "practice":
+                            practice = database.getRandomPractice(language)
+                            pronunTest = pronuntest.PronunTest(ctx=ctx, user=username, practice=practice, progressId=practice_id)
+                            hasQuestion, sentence, view = pronunTest.get_question()
+                            if hasQuestion:
+                                await ctx.send(f"How do you say: {sentence}", view=view)
+
+                if done_count == len(dblessons):
+                    embed.set_thumbnail(url="attachment://trophy.png")
+                    file = discord.File("images/trophy.png", filename="trophy.png")
+                    await interaction.response.defer()
+                    await my_msg.edit(content='', embed=progress_embed, view=view, file=file)
+                else:
+                    await interaction.response.defer()
+                    await my_msg.edit(content='', embed=progress_embed, view=view)
+
+                button.callback = button_record_callback
+                practice_shown = True
+                print(f"{practice_id}    {practice_type}")
+
+
+            async def endbutton_callback(interaction):
+                nonlocal view
+                progress_embed = discord.Embed(title="Until Next Time!", description="", color=0x800020)
+                await interaction.response.defer()
+                await my_msg.edit(content='', embed=progress_embed, view=None)
+                view.stop()
+
             for dbunit in dbprogress:
                 embed = discord.Embed(
                     title=dbunit["name"],
@@ -49,7 +183,7 @@ class MyProgress(commands.Cog):
                         done = "Completed"
                     embed.add_field(
                         name=f"Lesson {index + 1}",
-                        value=f"{dblesson['name']}                           {done}",
+                        value=f"{dblesson['name']}{done}",
                         inline=False
                     )
                     index +=1
@@ -83,7 +217,6 @@ class MyProgress(commands.Cog):
                     practice_shown = True
                     print(f"{practice_id}    {practice_type}")
                     await ctx.send(view=view)
-
 
         else:
             error_embed = discord.Embed(title="Language Not Selected", description=f"Please use command !changeLanguage [language] to select your language", color=0xFF0000)
